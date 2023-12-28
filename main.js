27f3c55e9beace2ab94095af900c3137ee815fe5
Madelen Yoo <rolemadelen@tuta.io>
2023-12-28 10:10:41 -0500

    reveal button added
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
+let isOpen = false;
 const buttons = document.querySelectorAll('button[data-state="closed"]');
 
 buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener('click', () => {
     const spanTag = button.nextElementSibling;
     spanTag.classList.toggle('blur');
     const state = button.getAttribute('data-state');
     button.setAttribute('data-state', state === 'open' ? 'closed' : 'open');
   });
 });
+
+const revealBtn = document.querySelector('.reveal > button');
+revealBtn.addEventListener('click', () => {
+  if (isOpen) closeAll();
+  else openAll();
+
+  revealBtn.innerText = isOpen ? 'open' : 'close';
+  isOpen = !isOpen;
+});
+
+function openAll() {
+  buttons.forEach((button) => {
+    const spanTag = button.nextElementSibling;
+    spanTag.classList.remove('blur');
+    button.setAttribute('data-state', 'open');
+  });
+}
+function closeAll() {
+  buttons.forEach((button) => {
+    const spanTag = button.nextElementSibling;
+    spanTag.classList.add('blur');
+    button.setAttribute('data-state', 'closed');
+  });
+}
